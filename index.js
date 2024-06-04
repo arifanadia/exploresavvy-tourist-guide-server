9bc13a3ca01c9047947837763f5ae9d69da8784d
arifanadia <contactarifanadia@gmail.com>
2024-06-04 19:01:52 +0600

    monog altas connected

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const cors = require('cors');
 const port = process.env.PORT || 5000
@@ -9,9 +10,52 @@ app.use(cors());
 app.use(express.json());
 
 
-app.get('/',(req,res)=> {
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.taokb31.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const packagesCollection = client.db('exploreSavvyDb').collection('packages');
+        const tourGuideCollection = client.db('exploreSavvyDb').collection('tourGuides');
+        const touristStoriesCollection = client.db('exploreSavvyDb').collection('touristStories');
+
+
+        // packages api
+
+        app.get('/packages',async(req,res)=>{
+            const result = await packagesCollection.find().toArray()
+            res.send(result)
+        })
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
     res.send('exploreSavvy are now exploring')
 })
-app.listen(port,(req,res)=>{
+app.listen(port, (req, res) => {
     console.log(`exploreSavvy are exploring on port : ${port}`);
 })
